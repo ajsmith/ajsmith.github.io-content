27d6fb098c99bc9fee05eda9de5e4e444c69b23e
Alex Smith <alex.smith@redhat.com>
2016-08-19 15:54:20 -0400

    Re-title.

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Alex Smith"  # (translatable)
-BLOG_TITLE = "Alex's Blog"  # (translatable)
+BLOG_TITLE = "Bits, Bytes, and Things That Compile in the Night"
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://ajsmith.github.io/"
